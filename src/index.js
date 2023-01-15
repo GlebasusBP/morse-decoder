805f8a6a30ed0160a9684833e0f360a5ce39d3f7
GlebasusBP <bgp13art@Gmail.com>
2023-01-15 11:37:18 +0300

    50 passing

--- src/index.js
+++ src/index.js
@@ -38,8 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  
+    let arr = [];
+    
+    let res = '';
+  
+    for(let i = 0; i < expr.length; i+=10 ){
+      arr.push(expr.slice(i, i+10));
+    }
+  
+    arr.forEach( e => {
+      let Arr = [];
+      let morzeKey = '';
+  
+      for(let i = 0; i < e.length; i+=2 ){
+        if( e == '**********'){
+          Arr.push('**');
+          break;
+        } else {
+          Arr.push(e.slice(i, i+2));
+        }  
+      }
+  
+      Arr.forEach(e => {
+        if(e == 10){
+          morzeKey += '.';
+        } else if(e == 11){
+          morzeKey += '-';
+        } else if(e == '**'){
+          morzeKey += 'space';
+        }
+      })
+  
+      if(morzeKey == 'space'){
+        res += ' ';
+      } else {
+        for(let key in MORSE_TABLE){
+          if(key == morzeKey){
+            res += MORSE_TABLE[key];
+          }
+        }
+      }
+    })
+    return res;
+  }
 
 module.exports = {
     decode
